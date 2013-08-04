6656c220b91cd024659f639100cfad2fb6a2fa1d
dewfalse <dewfalse@gmail.com>
2013-08-04 23:35:09 +0900

    筍成長率を1%に変更

--- Config.java
+++ Config.java
@@ -120,7 +120,7 @@ public class Config {
 			biomeID = cfg.get(Configuration.CATEGORY_GENERAL, "biomeID", 50).getInt();
 			玉鋼出現率 = cfg.get(Configuration.CATEGORY_GENERAL, "玉鋼出現率", 20).getInt();
 			鳥居出現率 = cfg.get(Configuration.CATEGORY_GENERAL, "鳥居出現率", 1.0D).getDouble(1.0D);
-			筍成長確率 = cfg.get(Configuration.CATEGORY_GENERAL, "筍成長確率", 5.0D).getDouble(5.0D);
+			筍成長確率 = cfg.get(Configuration.CATEGORY_GENERAL, "筍成長確率", 1.0D).getDouble(1.0D);
 			鍛練回数 = cfg.get(Configuration.CATEGORY_GENERAL, "鍛練回数", 2).getInt();
 			たたら製鉄炉レシピ簡易化 = cfg.get(Configuration.CATEGORY_GENERAL, "たたら製鉄炉レシピ簡易化", false).getBoolean(false);
 			cfg.save();
