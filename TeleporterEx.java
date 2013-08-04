68e860aea9e0b4c32ad079c75352247947adf717
dewfalse <dewfalse@gmail.com>
2013-08-05 01:08:12 +0900

    entityの転送メソッドを作成

--- TeleporterEx.java
+++ TeleporterEx.java
@@ -1,6 +1,7 @@
 package wa;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
@@ -31,4 +32,38 @@ public class TeleporterEx extends Teleporter {
 		}
 	}
 
+	public static void transferEntityToWorld(Entity par1Entity, int dim) {
+
+		if (!par1Entity.worldObj.isRemote && !par1Entity.isDead) {
+			MinecraftServer minecraftserver = MinecraftServer.getServer();
+			int dimOld = par1Entity.dimension;
+			WorldServer worldserver = minecraftserver.worldServerForDimension(dimOld);
+			WorldServer worldserver1 = minecraftserver.worldServerForDimension(dim);
+			par1Entity.dimension = dim;
+
+			par1Entity.worldObj.removeEntity(par1Entity);
+			par1Entity.isDead = false;
+			minecraftserver.getConfigurationManager().transferEntityToWorld(par1Entity, dimOld, worldserver, worldserver1, new TeleporterEx(worldserver1));
+			Entity entity = EntityList.createEntityByName(EntityList.getEntityString(par1Entity), worldserver1);
+
+			if (entity != null) {
+				entity.copyDataFrom(par1Entity, true);
+				int x = (int) par1Entity.posX;
+				int y = (int) par1Entity.posY;
+				int z = (int) par1Entity.posZ;
+				for (int j = y; j < 255; ++j) {
+					if (worldserver1.getBlockId(x, j - 1, z) != 0) continue;
+					if (worldserver1.getBlockId(x, j, z) != 0) continue;
+					if (worldserver1.getBlockId(x, j + 1, z) != 0) continue;
+					par1Entity.setLocationAndAngles(x, j + 2, z, 0, 0);
+					break;
+				}
+				worldserver1.spawnEntityInWorld(entity);
+			}
+
+			par1Entity.isDead = true;
+			worldserver.resetUpdateEntityTick();
+			worldserver1.resetUpdateEntityTick();
+		}
+	}
 }
