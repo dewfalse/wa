c6f1f0df3c1a2816a3cde4b47336b28b11262475
dewfalse <dewfalse@gmail.com>
2013-08-07 01:02:28 +0900

    稲のクラフト時の挙動を修正 #16

--- ItemIne.java
+++ ItemIne.java
@@ -1,28 +1,22 @@
 package wa;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import cpw.mods.fml.common.ICraftingHandler;
 
-public class ItemIne extends Item implements ICraftingHandler {
+public class ItemIne extends Item {
 
 	public ItemIne(int par1) {
 		super(par1);
 	}
 
 	@Override
-	public void onCrafting(EntityPlayer player, ItemStack item,
-			IInventory craftMatrix) {
-		if (item.itemID == Items.米.itemID) {
-			player.dropItem(Blocks.wara.blockID, item.stackSize);
-			player.addStat(Achievements.kome, 1);
-		}
+	public ItemStack getContainerItemStack(ItemStack itemStack) {
+		return new ItemStack(Blocks.wara);
 	}
 
 	@Override
-	public void onSmelting(EntityPlayer player, ItemStack item) {
+	public boolean hasContainerItem() {
+		return true;
 	}
 
 }
