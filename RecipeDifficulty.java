ef7cf67a5ac289ee4d2dcc862a88209413eca5e2
dewfalse <dewfalse@gmail.com>
2013-08-07 01:37:05 +0900

    レシピ難易度を定義

---
+++ RecipeDifficulty.java
@@ -0,0 +1,10 @@
+package wa;
+
+public class RecipeDifficulty {
+
+	public static final int ULTRA_HARD = 0;
+	public static final int HARD = 1;
+	public static final int NORMAL = 2;
+	public static final int EASY = 3;
+	public static final int VERY_EASY = 4;
+}
