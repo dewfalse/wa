df7f819448c631dca1452ffcc3af71f865af2088
dewfalse <dewfalse@gmail.com>
2013-09-06 01:00:18 +0900

    通常世界で村ブロック置換しないように

---
+++ BiomeGenWaPlains.java
@@ -0,0 +1,11 @@
+package wa;
+
+import net.minecraft.world.biome.BiomeGenPlains;
+
+public class BiomeGenWaPlains extends BiomeGenPlains {
+
+	public BiomeGenWaPlains(int par1) {
+		super(par1);
+	}
+
+}
